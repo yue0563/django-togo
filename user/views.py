b4c00623abc03ecf86842b7eed3522d7d85781e4
yu <tim233266@gmail.com>
2024-09-28 11:01:23 +0800

    完成login頁面

---
+++ user/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+
+
+# Create your views here.
+def user_login(request):
+    message = ""
+    user = None
+    if request.method == "POST":
+        if request.POST.get("register"):
+            return redirect("register")
+
+        if request.POST.get("login"):
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            if username == "" or password == "":
+                message = "帳號密碼不可為空!"
+            else:
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    message = "登入成功!"
+                    login(request, user)
+                else:
+                    message = "帳號或密碼錯誤!"
+
+    return render(request, "user/login.html", {"message": message, "user": user})
+
+
+def user_register(request):
+    message = ""
+    form = UserCreationForm()
+    # all,get,filter
+    print(User.objects.all())
+
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        # 密碼長度8
+        if len(password1) != 8 or len(password2) != 8:
+            message = "密碼長度不正確"
+        elif password1 != password2:
+            message = "兩次密碼不一樣"
+        else:
+            # 比對使用者是否存在
+            if User.objects.filter(username=username):
+                message = "帳號已存在!"
+            # 註冊使用者
+            else:
+                user = User.objects.create_user(username=username, password=password1)
+                user.save()
+                message = "註冊成功!"
+
+    return render(request, "user/register.html", {"form": form, "message": message})
